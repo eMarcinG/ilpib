eff7e0ef84e05b8a9abb68639a70b5efbdadf115
Marcin G <m.gumny@giap.pl>
2025-03-06 23:03:03 +0100

    permission for is author

--- ilpib/management/viewsets.py
+++ ilpib/management/viewsets.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, ip_address=self.request.META['REMOTE_ADDR'])
